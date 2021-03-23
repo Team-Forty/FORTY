88fb6c92c7590015180a2f1acb44d8cc8a54eda1
SteffenKloempges <steffen.kloempges@gmail.com>
2021-03-23 16:06:17 +0100

    steffen

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,15 @@ import "./Header.css";
 
 
 const Header = () => {
-    return ( 
+    return (
         <section className="header" >
             <h2 className="header_title">FORTY</h2>
             <h2 className="header_under"> MENU ...</h2>
-            <img src="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg" alt=""/>
-            <img src="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg" alt=""/>
+            <img src="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg" alt="" />
+            <img src="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg" alt="" />
+            <h2>Hier war Steffen</h2>
         </section>
-     );
+    );
 }
- 
+
 export default Header;
